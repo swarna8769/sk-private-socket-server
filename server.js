c94a26a113f98917ffb39be6979cb4946b019138
swarna8769 <rithuqr@gmail.com>
2025-05-12 11:20:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,50 +1,41 @@
-// server.js
-const express = require("express");
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
-app.use(cors());
-
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+  },
 });
 
-const users = {};
+const users = new Map();
 
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+  console.log("User connected");
 
   socket.on("register", ({ uid }) => {
-    users[uid] = socket.id;
-    console.log("Registered:", uid, socket.id);
+    users.set(uid, socket.id);
   });
 
   socket.on("private-message", ({ to, from, message }) => {
-    const toSocket = users[to];
-    if (toSocket) {
-      io.to(toSocket).emit("receive-private-message", { from, message });
+    const targetSocketId = users.get(to);
+    if (targetSocketId) {
+      io.to(targetSocketId).emit("receive-private-message", { from, message });
     }
   });
 
   socket.on("disconnect", () => {
-    for (const uid in users) {
-      if (users[uid] === socket.id) {
-        delete users[uid];
+    for (const [uid, id] of users.entries()) {
+      if (id === socket.id) {
+        users.delete(uid);
         break;
       }
     }
-    console.log("User disconnected:", socket.id);
   });
 });
 
-const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => {
-  console.log("Server listening on port", PORT);
+server.listen(10000, () => {
+  console.log("Server running on port 10000");
 });
